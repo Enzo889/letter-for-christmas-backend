12502a559d0d167b07b7c4ebf1f4b5c353cd713e
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-06 17:35:08 -0300

    feat: empty project

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! my word is your world. this is my new ca';
-  }
-}
